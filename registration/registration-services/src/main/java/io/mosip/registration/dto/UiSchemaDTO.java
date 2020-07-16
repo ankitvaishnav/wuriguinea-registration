7422532355b207fa9532ccab08efc372b5d9c6b8
Ferdinand Goumou <ferdinand.goumou@wuriguinee.com>
2020-07-16 16:51:12 +0000

    commit new design implementation

--- registration/registration-services/src/main/java/io/mosip/registration/dto/UiSchemaDTO.java
+++ registration/registration-services/src/main/java/io/mosip/registration/dto/UiSchemaDTO.java
@@ -17,12 +17,11 @@ import lombok.Setter;
  * This class is a DTO which parses with UI Schema Json, which decides of how
  * the UI element should behave like whether it is a TextField / Password /
  * Button.
- * 
+ * <p>
  * And also it contains the validators to validate the respective UI element,
  * label name,controlType, format,whether it is required or not.
- * 
- * @author YASWANTH S
  *
+ * @author YASWANTH S
  */
 @Data
 @AllArgsConstructor
@@ -30,32 +29,33 @@ import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UiSchemaDTO {
 
-	private String id;
-	private boolean inputRequired;
-	private String type;
-	private int minimum;
-	private int maximum;
-	private String description;
-	private HashMap<String, String> label;
-	private String controlType;
-	private String fieldType;
-	private String format;
-	private List<Validator> validators;
-	private String fieldCategory;
- 	
-	@JsonProperty("required")
-	private boolean isRequired;	
-	
-	@JsonProperty("bioAttributes")
-	private List<String> bioAttributes;
-	
-	@JsonProperty("requiredOn")
-	private List<RequiredOnExpr> requiredOn;
-	
-	@JsonProperty("subType")
-	private String subType;
-	
-	private String contactType;
-	private String group;
+    private String id;
+    private boolean inputRequired;
+    private String type;
+    private int minimum;
+    private int maximum;
+    private String description;
+    private HashMap<String, String> label;
+    private String controlType;
+    private String fieldType;
+    private String format;
+    private List<Validator> validators;
+    private String fieldCategory;
+    private String layoutCategory;
+
+    @JsonProperty("required")
+    private boolean isRequired;
+
+    @JsonProperty("bioAttributes")
+    private List<String> bioAttributes;
+
+    @JsonProperty("requiredOn")
+    private List<RequiredOnExpr> requiredOn;
+
+    @JsonProperty("subType")
+    private String subType;
+
+    private String contactType;
+    private String group;
 
 }
